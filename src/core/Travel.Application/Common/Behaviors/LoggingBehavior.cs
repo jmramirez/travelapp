ffa1586e77d849a9e39c4f088ef9b8866faed326
Jose Ramirez <josemramirez81@outlook.com>
2021-12-10 00:39:37 -0500

    Create MediatR pipeline behaviors

---
+++ src/core/Travel.Application/Common/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,23 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
+namespace Travel.Application.Common.Behaviors
+{
+    public class LoggingBehavior<TRequest> : IRequestPreProcessor<TRequest>
+    {
+        private readonly ILogger _logger;
+
+        public LoggingBehavior(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+            _logger.LogInformation("Travel Request: {@Request}", requestName, request);
+        }
+    }
+}
