62c2c918cfc3d3378f554f781597197114e4f585
Jose Ramirez <josemramirez81@outlook.com>
2021-12-15 00:35:59 -0500

    Define interfaces or contracts to be used instead of the actual classes

---
+++ src/core/Travel.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Travel.Domain.Entities;
+
+namespace Travel.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<TourList> TourLists { get; set; }
+        DbSet<TourPackage> TourPackages { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+
+    }
+}
