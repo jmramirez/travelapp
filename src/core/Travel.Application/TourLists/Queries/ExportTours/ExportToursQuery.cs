438a93f2e5dca0ac5e853975be7c2d50b9433d7e
Jose Ramirez <josemramirez81@outlook.com>
2021-12-15 00:41:15 -0500

    Create queries for TourLists, which  are requests for reading data, and a query handler

---
+++ src/core/Travel.Application/TourLists/Queries/ExportTours/ExportToursQuery.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Travel.Application.Common.Interfaces;
+
+namespace Travel.Application.TourLists.Queries.ExportTours
+{
+    public class ExportToursQuery : IRequest<ExportToursVm>
+    {
+        public int ListId { get; set; }
+    }
+
+    public class ExportToursQueryHandler : IRequestHandler<ExportToursQuery, ExportToursVm>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly ICsvFileBuilder _fileBuilder;
+
+        public ExportToursQueryHandler(IApplicationDbContext context, IMapper mapper, ICsvFileBuilder fileBuilder)
+        {
+            _mapper = mapper;
+            _context = context;
+            _fileBuilder = fileBuilder;
+        }
+
+        public async Task<ExportToursVm> Handle(ExportToursQuery request, CancellationToken cancellationToken)
+        {
+            var vm = new ExportToursVm();
+
+            var records = await _context.TourPackages
+                .Where(t => t.ListId == request.ListId)
+                .ProjectTo<TourPackageRecord>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
+            vm.Content = _fileBuilder.BuildTourPackagesFile(records);
+            vm.ContentType = "text/cvs";
+            vm.FileName = "TourPackages.csv";
+
+            return await Task.FromResult(vm);
+
+        }
+    }
+}
---
+++ src/core/Travel.Application/TourLists/Queries/ExportTours/TourPackageRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Travel.Application.Common.Mapping;
+using Travel.Domain.Entities;
+
+namespace Travel.Application.TourLists.Queries.ExportTours
+{
+    public class TourPackageRecord : ImapFrom<TourPackage>
+    {
+        public string Name { get; set; }
+        public string MapLocation { get; set; }
+    }
+}
