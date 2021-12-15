186fc9ad150767eeb2acd42a56bb9fa8c8195a54
Jose Ramirez <josemramirez81@outlook.com>
2021-12-15 00:39:09 -0500

    Create commands for TourLists and TourPackage entities, which is are requests for saving, updating, or deleting data

---
+++ src/core/Travel.Application/TourLists/Commands/DeleteTourList/DeleteTourListCommand.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Travel.Application.Common.Exceptions;
+using Travel.Application.Common.Interfaces;
+
+namespace Travel.Application.TourLists.Commands.DeleteTourList
+{
+    public class DeleteTourListCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+
+    public class DeleteTourListCommandHandler : IRequestHandler<DeleteTourListCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteTourListCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteTourListCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.TourLists
+                .Where(l => l.Id == request.Id)
+                .SingleOrDefaultAsync(cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(TourLists), request.Id);
+            }
+
+            _context.TourLists.Remove(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+            
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/core/Travel.Application/TourPackages/Commands/UpdateTourPackage/UpdateTourPackage.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Travel.Application.Common.Exceptions;
+using Travel.Application.Common.Interfaces;
+using Travel.Application.TourLists.Commands.UpdateTourList;
+using Travel.Domain.Entities;
+
+namespace Travel.Application.TourPackages.Commands.UpdateTourPackage
+{
+    public class UpdateTourPackageCommand : IRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class UpdateTourPackageCommandHandler : IRequestHandler<UpdateTourPackageCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateTourPackageCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateTourPackageCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.TourPackages.FindAsync(request.Id);
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(TourPackage), request.Id);
+            }
+
+            entity.Name = request.Name;
+            await _context.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
