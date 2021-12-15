7cc07ee22c721ba76be7c5e7c2ea8883c943f094
Jose Ramirez <josemramirez81@outlook.com>
2021-12-15 00:31:08 -0500

    Setup AutoMapper

---
+++ src/core/Travel.Application/Common/Mappings/ImapFrom.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Travel.Application.Common.Mapping
+{
+    public interface ImapFrom<T>
+    {
+        void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+    }
+}
