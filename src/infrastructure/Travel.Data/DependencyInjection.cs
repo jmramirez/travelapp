574fce85d1bbfa99c066548b9cda229c43243e6e
Jose Ramirez <josemramirez81@outlook.com>
2021-12-16 00:47:50 -0500

    Refactor code to solve bugs

---
+++ src/infrastructure/Travel.Data/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Travel.Application.Common.Interfaces;
+using Travel.Data.Contexts;
+
+namespace Travel.Data;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureData(this IServiceCollection services)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql("host=localhost;database=travelapp_dev;username=postgres;password=devapplication"));
+
+        services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+        return services;
+    }
+}
