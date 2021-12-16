47ba037e16c2645538df6f896ca69b3e76d4bdba
Jose Ramirez <josemramirez81@outlook.com>
2021-12-16 01:37:27 -0500

    Create entity User

---
+++ src/core/Travel.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Travel.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
